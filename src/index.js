33b0f18ad6b2f1138ad02e8a56c543fcb2b21e65
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-25 17:55:57 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  document
+    .getElementById('create-task-form')
+    .addEventListener('submit', (e) => {
+      const taskDescription = e.target.new_task_description.value;
+
+      const newTask = document.createElement('p');
+      newTask.textContent = `${taskDescription} `;
+
+      const deleteKey = document.createElement('button');
+      deleteKey.textContent = 'x';
+      newTask.appendChild(deleteKey);
+
+      document.getElementById('tasks').appendChild(newTask);
+      e.preventDefault();
+
+      deleteKey.addEventListener('click', (e) => {
+        e.target.parentNode.remove();
+      });
+    });
 });
